ff017e36cb5603a98bd172c53bfe149ab8d3877f
carlosmast23 <carlosmast2301@hotmail.es>
2015-12-15 08:40:31 -0500

    primera version

---
+++ ClienteSocketCSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using ClienteSocketCSharp.socket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteSocketCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocketCliente sc = new SocketCliente("127.0.0.1",9999);
+            Console.WriteLine("presione una tecla para iniciar la conexion ...");
+            //Console.ReadLine();
+            Console.WriteLine("cliente conectado con el servidor...");
+            sc.conectar();
+            while (true)
+            {
+                Console.WriteLine("Ingrese el mensaje para enviar: ");
+                String msj = Console.ReadLine();
+                sc.salida(msj);
+                if (msj.Equals("EXIT"))
+                    break;
+                else
+                    Console.WriteLine(sc.leer());
+            }
+        }
+    }
+}
---
+++ ClienteSocketCSharp/socket/SocketCliente.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteSocketCSharp.socket
+{
+    class SocketCliente
+    {
+        private Socket clientSocket;
+        private IPEndPoint serverAddress;
+
+        private String host;
+        private int puerto;
+
+        public SocketCliente(String host, int puerto)
+        {
+            this.host = host;
+            this.puerto = puerto;
+            //this.conectar();
+        }
+
+        public void conectar()
+        {
+            serverAddress = new IPEndPoint(IPAddress.Parse(host), puerto);
+
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(serverAddress);
+        }
+
+        public String leer()
+        {
+            byte[] sizeBuffer = new byte[1024];
+            int tamanio = clientSocket.Receive(sizeBuffer);
+            String myString = Encoding.UTF8.GetString(sizeBuffer, 0, tamanio);
+            return myString;
+        }
+
+        public Boolean salida(String texto)
+        {
+            byte[] msg = Encoding.UTF8.GetBytes(texto);
+            clientSocket.Send(msg);
+            return true;
+        }
+
+        public void desconectar()
+        {
+            clientSocket.Close();
+        }
+    }
+}
